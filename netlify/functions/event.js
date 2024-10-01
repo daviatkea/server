ed065befcae2cf7d8e21b956fd125714433df688
Dannie Vinther <davi@kea.dk>
2024-10-01 08:00:17 +0200

    add event endpoint

---
+++ netlify/functions/event.js
@@ -0,0 +1,26 @@
+exports.handler = async (event) => {
+  const data = require("../../api/data.json");
+  const id = event.queryStringParameters.id;
+
+  const artist = data.events.find((a) => a.id === parseInt(id));
+
+  if (artist) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(artist),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Event ikke fundet" }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  }
+};
